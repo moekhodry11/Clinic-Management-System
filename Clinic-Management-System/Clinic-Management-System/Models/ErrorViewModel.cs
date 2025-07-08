cac1e12d9d5233f14204c9288eec4b2c077729ad
moekhodry11 <86708003+moekhodry11@users.noreply.github.com>
2025-07-08 17:41:13 +0300

    rearrange folders

--- Clinic-Management-System/Clinic-Management-System/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Clinic_Management_System.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
